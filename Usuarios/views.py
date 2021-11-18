68a65eda8ff4ec8308f4c232b280f644c685da1c
Jorgelina Fernandez <mickyomega7@gmail.com>
2021-11-18 02:29:08 -0300

    ultima subida

--- Usuarios/views.py
+++ Usuarios/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
+
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from .models import User
 
 ## Formulario para creación de usuario
 class CreateUserForm(UserCreationForm):
